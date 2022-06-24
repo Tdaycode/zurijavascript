150a64df7fb771ad949ccdeda429dd1c2de89c1e
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-06-24 12:00:29 +0100

    adding

---
+++ script.js
@@ -0,0 +1,11 @@
+function printMe() {
+  alert("Omotayo Ganiyu");
+  console.log("Omotayo Ganiyu");
+  alert("I am 173cm tall")
+  console.log("I am 173cm tall")
+  alert("Nigeria")
+  console.log("Nigeria")
+}
+
+
+printMe()
